825f2603f36c19923e685ed26a76e711265c4d02
kjb95 <topmedia44@gmail.com>
2022-11-01 18:24:03 +0900

    feat: 지하철 노선 정보 초기값 세팅하기 (노선 등록)

--- src/main/java/subway/model/LineRepository.java
+++ src/main/java/subway/model/LineRepository.java
@@ -1,4 +1,4 @@
-package subway.domain;
+package subway.model;
 
 import java.util.ArrayList;
 import java.util.Collections;
@@ -7,7 +7,16 @@ import java.util.Objects;
 
 public class LineRepository {
 
+    private static final String LINE_TWO = "2호선";
+    private static final String LINE_THREE = "3호선";
+    private static final String SHIN_BUN_DANG_LINE = "신분당선";
     private static final List<Line> lines = new ArrayList<>();
+
+    public LineRepository() {
+        addLine(new Line(LINE_TWO));
+        addLine(new Line(LINE_THREE));
+        addLine(new Line(SHIN_BUN_DANG_LINE));
+    }
 
     public static List<Line> lines() {
         return Collections.unmodifiableList(lines);
